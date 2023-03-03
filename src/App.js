3dc059a45d4caf32c1792f51507630271b4d034c
Marlon Baker <marbaker.swe@gmail.com>
2023-03-02 22:59:32 -0800

    Navbar v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import InstaNav from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <InstaNav />
     </div>
   );
 }
---
+++ src/components/Navbutton.jsx
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+function OutlineTypesExample() {
+  return (
+    <>
+      <Button variant="light">Log in</Button>{' '}
+      <Button variant="success">Sign up</Button>{' '}
+    </>
+  );
+}
+
+export default OutlineTypesExample;
